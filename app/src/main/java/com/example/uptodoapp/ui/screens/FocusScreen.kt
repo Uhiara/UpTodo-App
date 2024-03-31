981020d1aefc4e6ea5ebd50806835aa9867b1e19
Uhiara-Chidiadi <uhiarachidiadi@gmail.com>
2024-03-31 11:01:25 +0100

    Merge remote-tracking branch 'origin/HomeScreen' into HomeScreen

--- app/src/main/java/com/example/uptodoapp/ui/screens/FocusScreen.kt
+++ app/src/main/java/com/example/uptodoapp/ui/screens/FocusScreen.kt
@@ -1,14 +1,23 @@
 package com.example.uptodoapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.uptodoapp.R
+import com.example.uptodoapp.ui.common.CommonText
 
 @Composable
 fun FocusScreen() {
@@ -21,7 +30,32 @@ fun FocusScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Focus Screen")
+            Image(
+                painter = painterResource(id = R.drawable.checklist),
+                contentDescription = "Checklist"
+            )
+
+            CommonText(
+                modifier = Modifier,
+                textValue = "What do you want to do today?",
+                textSize = 20.sp,
+                lineHeight = 30.sp,
+                fontWeight = FontWeight(400),
+                colorValue = colorResource(id = R.color.text_medium),
+                fontFamily = FontFamily(Font(R.font.roboto_regular)),
+                textAlign = TextAlign.Center
+            )
+
+            CommonText(
+                modifier = Modifier,
+                textValue = "Tap + to add your tasks",
+                textSize = 16.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(400),
+                colorValue = colorResource(id = R.color.text_medium),
+                fontFamily = FontFamily(Font(R.font.roboto_regular)),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
